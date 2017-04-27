c402d55f26681ef731954709beb0fb56c36c3211
ADITYAMOHAN K V <adityamohankv@users.noreply.github.com>
2017-04-27 16:28:51 +0530

    Update ReduceClass.java

--- MapReduce/MapReduce/src/analysis12/ReduceClass.java
+++ MapReduce/MapReduce/src/analysis12/ReduceClass.java
@@ -23,7 +23,7 @@ public class ReduceClass extends Reducer <Text,Text,NullWritable,Text>{
 		}
 		double sucess_rate = (sucess/total)*100;
 		if (sucess_rate >= 70 && total >=1000){
-			String output = key.toString() +","+String.format("%.0f",total)+","+String.format("%.2f %%",sucess_rate);
+			String output = key.toString() +"\t"+String.format("%.0f",total)+"\t"+String.format("%.2f %%",sucess_rate);
 			top10.put(sucess_rate, output);				
 		}
 	}
